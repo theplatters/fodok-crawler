2012f18bb10e65a9a433b37237b89b02fe3b779e
theplatters <fscharnreitner@gmail.com>
2024-11-10 11:32:03 +0100

    things

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"][clj-http "3.12.3"][org.clojure/data.csv "1.1.0"]]
   :repl-options {:init-ns fodok-crawler.core}
-  :plugins [[cider/cider-nrepl "0.42.1"]])
+  :plugins [[cider/cider-nrepl "0.42.1"]]
+  :main fodok-crawler.core
+  :aot [fodok-crawler.core])
--- src/fodok_crawler/core.clj
+++ src/fodok_crawler/core.clj
@@ -1,41 +1,81 @@
-(ns fodok-crawler.core)
+(ns fodok-crawler.core
+  (:gen-class))
 
 (require '[clj-http.client :as client]
          '[clojure.xml :as xml]
-         '[clojure.zip :as zip]
          '[clojure.data.csv :as csv]
          '[clojure.java.io :as io])
 
-(defn zip-str [s]
-  (zip/xml-zip 
-      (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+(defn parse_to_xml [response]
+  (-> response
+      :body
+      .getBytes
+      java.io.ByteArrayInputStream.
+      xml/parse
+      (get-in [:content 1 :content 0 :content])))
 
-(def response (client/get "https://fodok.jku.at/fodok/forschungseinheit_typo3.xsql?FE_ID=348&xml-stylesheet=none"))
+(defn filter_for_tag [tag content]
+  (->> content
+       (filter #(= (:tag %) tag))
+       first
+       :content
+       first))
 
+(defn destructure_publication [publication_row]
+  (let [content (:content publication_row)]
+    {:title (filter_for_tag :TITEL content)
+     :authors (filter_for_tag :AUTOREN_ZITAT content)
+     :year (filter_for_tag :ERSCHEINUNGSJAHR content)
+     :citation (filter_for_tag :ZITAT_DE content)
+     :id (filter_for_tag :PUB_ID content)}))
 
-(def content (let [data (xml/parse 
-          (java.io.ByteArrayInputStream. (.getBytes (get response :body))))]
-(get-in data [:content 1 :content 0 :content])))
+(defn destructure_talk [talk_row]
+  (let [content (:content talk_row)]
+    {:title (filter_for_tag :TITEL content)
+     :date (filter_for_tag :DATUM content)}))
 
+(defn destructure_reasearch_project [rp_row]
+  (let [content (:content rp_row)]
+    {:name (filter_for_tag :BEZEICHNUNG content)}))
 
-(defn get_publications [content_map]
-  (let [_,_,content] (:content content_map)
-    content))
+(defn destructure_type [type_of_content, content]
+  (let [destructure_fun (case type_of_content
+                          :VORTRAGSTYPEN destructure_talk
+                          :PUBLIKATIONSTYPEN destructure_publication
+                          :FORSCHUNGSPROJEKTTYPEN destructure_reasearch_project)
+        type_title (-> content :content first :content first)
+        rows (-> content :content (nth 2) :content)]
+    (map #(assoc (destructure_fun %1) :type type_title) rows)))
 
+(defn write-csv [path row-data columns]
+  (let [headers (map name columns)
+        rows (mapv #(mapv % columns) row-data)]
+    (with-open [file (io/writer path)]
+      (csv/write-csv file (cons headers rows)))))
 
-(defn get_specific_contents [content type_of_content]
-  (get (first (filter #(= (get %1 :tag) type_of_content) content)) :content)
-  )
+(defn destructure_types [type_of_content content]
+  (map #(destructure_type type_of_content %) content))
 
+(defn get_specific_contents [type_of_content content]
+  (->> content
+       (filter #(= (:tag %) type_of_content))
+       first
+       :content
+       (destructure_types type_of_content)
+       flatten))
 
+(def content
+  (-> "https://fodok.jku.at/fodok/forschungseinheit_typo3.xsql?FE_ID=348&xml-stylesheet=none"
+      client/get
+      parse_to_xml))
 
+(def talks (future (get_specific_contents :VORTRAGSTYPEN content)))
 
-(def talks (get_specific_contents content :VORTRAGSTYPEN))
-(def publications (get_specific_contents content :PUBLIKATIONSTYPEN))
-(def research_projcets (get_specific_contents content :FORSCHUNGSPROJEKTTYPEN))
+(def publications (future (get_specific_contents :PUBLIKATIONSTYPEN content)))
 
-(let [tags (xml/element :foo {:foo-attr "foo value"}
-             (xml/element :bar {:bar-attr "bar value"}
-               (xml/element :baz {} "The baz value")))]
-  (with-open [out-file (io/writer "foo.xml")]
-    (xml/emit tags out-file)))
+(def research_projcets (future (get_specific_contents :FORSCHUNGSPROJEKTTYPEN content)))
+
+(defn -main [& args]
+  (write-csv "publications.csv" (deref publications) [:authors :title :year :publication_type :citation])
+  (write-csv "talks.csv" (deref talks) [:title :date])
+  (write-csv "research_projcets.csv" (deref research_projcets) [:name]))
