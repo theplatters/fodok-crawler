370f4481810b9fd8873a80ccbfd6774447e88b76
theplatters <fscharnreitner@gmail.com>
2024-12-10 11:37:06 +0100

    cleanup

--- src/latextify/latextify.rb
+++ src/latextify/latextify.rb
@@ -1,12 +1,20 @@
 require 'csv'
 
-latex = "\\begin{itemize}\n"
-
-CSV.foreach('data/publications.csv', headers: true) do |row|
-  latex += "\\item #{row['title']}: #{row['authors']} #{row['citation']}\n"
+def generate_latex file, filename
+  latex = "\\begin{itemize}\n"
+  file.each do |row|
+    latex += "\\item #{row['title']}: #{row['authors']} #{row['citation']} - #{row["type"]}\n"
+  end
+  File.write(filename,latex)
 end
 
-latex += "\\end{itemize}\n"
+working_papers, finished_papers = CSV.read('data/publications.csv', headers: true)
+  .sort_by {|row| [row['year'].to_i, row['authors']]}
+  .reverse
+  .partition {|row| row["type"] == "Working Paper"}
 
-File.write('data/output.tex', latex)
-puts 'LaTeX itemize list generated in output.tex'
+generate_latex(working_papers,"data/working_papers.tex")
+puts 'LaTeX itemize list generated in working_papers.tex'
+generate_latex(working_papers,"data/publications.tex")
+puts 'LaTeX itemize list generated in publications.tex'
+
