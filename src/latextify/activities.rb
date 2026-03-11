a995528e345826dc9be3a16d275c6e79dd77bd2e
theplatters <fscharnreitner@gmail.com>
2026-03-11 14:12:00 +0100

    activities

--- src/latextify/activities.rb
+++ src/latextify/activities.rb
@@ -6,6 +6,7 @@ require 'date'
 require_relative 'helper_methods'
 
 TO_EXCLUDE = ['Andere Gutachtertätigkeit',
+              'Begutachtung von Publikationen und Herausgebertätigkeiten',
               'Begutachtung von Publikationen',
               'Begutachtung von Publikationen oder Herausgebertätigkeit (Altdaten)',
               'Gutachter/in für Förderinstitution',
@@ -14,36 +15,53 @@ TO_EXCLUDE = ['Andere Gutachtertätigkeit',
               'Programm-Komitee',
               'Sonstige Mitgliedschaft/Funktion',
               'Teilnahme an Konferenz, Workshop, ...',
+              'Teilnahme an oder Organisation einer Veranstaltung',
+              'Mitgliedschaft/Funktion',
+              'Gutachtertätigkeit',
+              'Gutachtertätigkeiten',
               'Wissenschaftliche Gesellschaft'].freeze
 
+def sort_by_organisator_then_alphabetically(acts)
+  acts.sort do |a, b|
+    next 1 if a['Rollen der Mitwirkenden'] == 'Moderator*in' && b['Rollen der Mitwirkenden'] != 'Moderator*in'
+    next 1 if a['Rollen der Mitwirkenden'] == 'Vortragende*r' && b['Rollen der Mitwirkenden'] != 'Vortragende*r'
+
+    a['Personen'].to_s <=> b['Personen'].to_s
+  end
+  acts
+end
+
+def build_persons(act)
+  extern_person = act.map { _1['Externe Person'] }.join(' ').strip
+  persons = act.map { _1['Personen'] }.join(' ').strip
+
+  "#{persons}#{extern_person.empty? ? '' : " #{extern_person}"}:"
+end
+
 def build_item_for_activities(title, act)
-  start_date = Date.parse(act.first['Startdatum']).strftime('%d.%m.%Y')
   cont = [
-    act.map { _1['Personen'] }.join(' '),
-    act.first['Externe Personen'],
-    act.first['Titel'],
+    build_persons(act),
     title[0],
-    start_date
+    act.first['Title'],
+    Date.parse(act.first['Startdatum']).strftime('%d.%m.%Y')
   ].join(' ')
 
   "\t\\item #{sanitize(cont)}"
 end
 
 def by_title_date(row)
-  [row['Title'], row['Startdatum']]
+  [row['Titel'], row['Startdatum']]
 end
 
 def research_seminar_predicate(row)
   !(row['Title'].to_s.include? 'Research' and row['Title'].to_s.include? 'ICAE')
 end
 
-def build_subsection(type, activities)
-  subsection = "\\subsection*{#{sanitize(type)}}\n"
+def build_subsection(_type, activities)
   items = activities.group_by { |r| by_title_date(r) }.map do |title, act|
     build_item_for_activities(title, act)
   end.join("\n")
-  subsection + "
-\\begin{enumerate}
+  "\\begin{enumerate}
 #{items}
 \\end{enumerate}
 "
